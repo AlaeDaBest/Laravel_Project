3053ea04551ec51f2cd2c50ed8b44e23ae723bc9
ImaneLyachouti <liemyy70@gmail.com>
2025-02-16 22:16:49 +0100

    Add files via upload

---
+++ ListFavorites.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "../Home/Header";
+import Fragrance from "../Fragrance/Fragrance";
+
+
+const ListFavorite = () => {
+  const [cart, setCart] = useState([]); 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const indexOfLastFragrance = currentPage * itemsPerPage;
+  const indexOfFirstFragrance = indexOfLastFragrance - itemsPerPage;
+  const currentCart = cart.slice(indexOfFirstFragrance, indexOfLastFragrance);
+  const totalPages = Math.ceil(cart.length / itemsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="body">
+      <Header />
+      <h1 className="title">Your Favorite List</h1>
+      <div id="CardGlobal">
+        {currentCart.length === 0 ? (
+          <p>Your Favorite list is empty.</p>
+        ) : (
+          currentCart.map((fragrance) => (
+            <div key={fragrance.id}>
+              <Fragrance fragrance={fragrance} />
+              <button
+                onClick={() => handleRemove(fragrance.id)}
+                className="RemoveButton"
+              >
+                Remove from favorite
+              </button>
+            </div>
+          ))
+        )}
+      </div>
+
+      {/* Pagination */}
+      <div className="pagination">
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="pagination-btn"
+        >
+          Prev
+        </button>
+        <span className="pagination-info">{`Page ${currentPage} of ${totalPages}`}</span>
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="pagination-btn"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ListFavorite;
